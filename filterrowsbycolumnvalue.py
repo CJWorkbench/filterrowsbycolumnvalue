b5c21f2a4b249a0cbc2bd65a5bd1b0bfd8c068b3
alexgonca <alexandre.reportagem@gmail.com>
2017-07-26 19:23:21 -0700

    initialize repo

---
+++ filterrowsbycolumnvalue.py
@@ -0,0 +1,28 @@
+class Importable:
+    @staticmethod
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def event():
+        pass
+
+    @staticmethod
+    def render(wf_module, table):
+        column = wf_module.get_param_string('column')
+        value = wf_module.get_param_string('value')
+        typeoffilter = wf_module.get_param_menu_string('typeoffilter')
+
+        if column == '' or value == '':
+            wf_module.set_ready(notify=False)
+            return None
+        elif column not in table.columns:
+            wf_module.set_error('Invalid column.')
+            return None
+        else:
+            if typeoffilter == 'Include rows by column value':
+                newtab = table.loc(table[column] == value)
+            else:
+                newtab = table.loc(table[column] != value)
+            wf_module.set_ready(notify=False)
+            return newtab
