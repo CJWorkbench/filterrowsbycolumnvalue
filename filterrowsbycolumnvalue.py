d06ab558ec2b7094d5c505686ac8ebea0f819fd3
Jonathan Stray <jonathanstray@gmail.com>
2017-11-21 19:17:17 -0800

    Update to new API

--- filterrowsbycolumnvalue.py
+++ filterrowsbycolumnvalue.py
@@ -1,28 +1,13 @@
-class Importable:
-    @staticmethod
-    def __init__(self):
-        pass
+def render(table, params):
+    column = params['column']
+    value = params['value']
+    typeoffilter = params['typeoffilter']
 
-    @staticmethod
-    def event():
-        pass
-
-    @staticmethod
-    def render(wf_module, table):
-        column = wf_module.get_param_column('column')
-        value = wf_module.get_param_string('value')
-        typeoffilter = wf_module.get_param_menu_string('typeoffilter')
-
-        if column == '' or value == '':
-            wf_module.set_ready(notify=False)
-            return table
-        elif column not in table.columns:
-            wf_module.set_error('Invalid column.')
-            return table
+    if column == '' or value == '':
+        return table
+    else:
+        if typeoffilter == 'Keep':
+            newtab = table.loc[table[column].astype(str) == value]
         else:
-            if typeoffilter == 'Keep':
-                newtab = table.loc[table[column].astype(str) == value]
-            else:
-                newtab = table.loc[table[column].astype(str) != value]
-            wf_module.set_ready(notify=False)
-            return newtab
+            newtab = table.loc[table[column].astype(str) != value]
+        return newtab
